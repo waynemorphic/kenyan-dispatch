2e31f27e7d4b46adccbfce2b55ad639082ee84ea
waynemorphic <wayneklyde@gmail.com>
2022-05-01 20:24:19 +0300

    heading as a variable block

--- application/views.py
+++ application/views.py
@@ -7,4 +7,6 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    heading = 'Kenyan Dispatch'
+    return render_template('index.html', heading = heading)
