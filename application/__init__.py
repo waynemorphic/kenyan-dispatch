af435b4fd14da0be42cfa3c5f918fe423f524d4d
waynemorphic <wayneklyde@gmail.com>
2022-05-01 20:56:46 +0300

    uninterrupted flask additions

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,13 @@
+
 from flask import Flask
-from config import DevConfig
+from .config import DevConfig
 
 # Application initialization
 app = Flask(__name__)
 
 # setting up configurations
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 
 from application import views
 
