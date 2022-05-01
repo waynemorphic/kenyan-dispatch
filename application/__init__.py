8396e9dd66ae49b9d7646f38f8fda74d1ca7d806
waynemorphic <wayneklyde@gmail.com>
2022-05-01 20:24:54 +0300

    cache commit

--- application/__init__.py
+++ application/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from config import DevConfig
 
 # Application initialization
 app = Flask(__name__)
+
+# setting up configurations
+app.config.from_object(DevConfig)
 
 from application import views
 
