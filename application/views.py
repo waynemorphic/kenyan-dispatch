93f91a743d4ca57db42241d8c438c2ef8a2e5de7
waynemorphic <wayneklyde@gmail.com>
2022-05-02 18:20:37 +0300

    cases to render responses to the template

--- application/views.py
+++ application/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from application import app
+from .request import get_article, get_news_source
 
 # views
 @app.route('/')
@@ -8,5 +9,14 @@ def index():
     View root page function that returns the index page and its data
     '''
 
+    # retrieving news source from the API with name and id
     heading = 'Kenyan Dispatch'
-    return render_template('index.html', heading = heading)
+
+    wired = get_news_source('wired')
+    article_author = get_article('author')
+    article_title = get_article('title')
+    article_description = get_article('description')
+    article_url = get_article('url')
+    article_urlToImage = get_article('urlToImage')
+  
+    return render_template('index.html', heading = heading, wired = wired, author = article_author, title = article_title, description = article_description, url = article_url, urlToImage = article_urlToImage)
