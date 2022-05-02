6b81e5f6e76bf133ee7b099b4ba1dc298cfa5f18
waynemorphic <wayneklyde@gmail.com>
2022-05-02 19:33:52 +0300

    changes to the source url

--- application/views.py
+++ application/views.py
@@ -12,11 +12,16 @@ def index():
     # retrieving news source from the API with name and id
     heading = 'Kenyan Dispatch'
 
-    wired = get_news_source('wired')
+    # sources
+    source_id = get_news_source('id')
+    source_name = get_news_source('name')
+    
+
+    # articles
     article_author = get_article('author')
     article_title = get_article('title')
     article_description = get_article('description')
     article_url = get_article('url')
     article_urlToImage = get_article('urlToImage')
   
-    return render_template('index.html', heading = heading, wired = wired, author = article_author, title = article_title, description = article_description, url = article_url, urlToImage = article_urlToImage)
+    return render_template('index.html', heading = heading, id = source_id, name = source_name, author = article_author, title = article_title, description = article_description, url = article_url, urlToImage = article_urlToImage)
